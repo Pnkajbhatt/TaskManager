49a08d00d9ec7d7619f86a6a8ef8520a77d7af38
pankaj bhatt <211pankajbhatt@gmail.com>
2026-06-25 21:23:27 +0530

    first commit

---
+++ TaskManagerbackend/TaskManagerbackend/src/main/java/in/Geekbits/TaskManagerbackend/TaskManagerbackendApplication.java
@@ -0,0 +1,13 @@
+package in.Geekbits.TaskManagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerbackendApplication.class, args);
+	}
+
+}
---
+++ TaskManagerbackend/TaskManagerbackend/src/test/java/in/Geekbits/TaskManagerbackend/TaskManagerbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package in.Geekbits.TaskManagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
